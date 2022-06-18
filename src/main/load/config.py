b00f506b40d468b47d591d34098c4c49ea84ed24
r0m1mPL <simachenko.roma@gmail.com>
2022-06-18 13:51:38 +0300

    update

--- src/main/load/config.py
+++ src/main/load/config.py
@@ -1,7 +1,5 @@
 from django.conf import settings
-from pydantic import BaseModel, Field
 import os
-from typing import Optional
 
 REGIONS_PATH = str(settings.BASE_DIR) + "/src/main/load/data/regions.xlsx"
 
@@ -14,36 +12,3 @@ UNIVERSITY_URL = "https://registry.edbo.gov.ua/api/universities/?ut=1&lc={}&exp=
 UNIVERSITIES_PATH = str(settings.BASE_DIR) + "/src/main/load/data/universities"
 
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY", "")
-
-
-class University(BaseModel):
-    name: str = Field(alias='university_name')
-    id: int = Field(alias='university_id')
-    parent_id: Optional[int] = Field(alias='university_parent_id')
-    short_name: str = Field(alias='university_short_name')
-    name_en: str = Field(alias='university_name_en')
-    is_from_crimea: str = Field(alias='is_from_crimea')
-    registration_year: Optional[int] = Field(alias='registration_year')
-    edrpou: int = Field(alias='university_edrpou')
-    type_name: str = Field(alias='university_type_name')
-    financing: str = Field(alias='university_financing_type_name')
-    governance: str = Field(alias='university_governance_type_name')
-    post_index: int = Field(alias='post_index')
-    koatuu_id: int = Field(alias='koatuu_id')
-    region_name: str
-    koatuu_name: str
-    address: str = Field(alias='university_address')
-    post_index_u: int = Field(alias='post_index_u')
-    koatuu_id_u: int = Field(alias='koatuu_id_u')
-    region_name_u: str = Field(alias='region_name_u')
-    koatuu_name_u: Optional[str] = Field(alias='koatuu_name_u')
-    address_u: str = Field(alias='university_address_u')
-    phone: str = Field(alias='university_phone')
-    email: str = Field(alias='university_email')
-    site: str = Field(alias='university_site')
-    director_post: str = Field(alias='university_director_post')
-    director_fio: str = Field(alias='university_director_fio')
-    close_date: Optional[str] = Field(alias='close_date')
-    primitki: str = Field(alias='primitki')
-
-
--- src/main/load/load_db.py
+++ src/main/load/load_db.py
@@ -7,11 +7,46 @@ import os
 from django.core.exceptions import ObjectDoesNotExist
 from .config import UNIVERSITIES_PATH
 from .exceptions import CantDownloadCertainUniversity, CantCreateNewUniversityObjectInDB, CantGetPlaceIDandCoordinates
-from .config import University
+from typing import Optional
+from pydantic import BaseModel, Field
 
 
-def process_regions() -> None:
-    """Processes regions"""
+class University(BaseModel):
+    name: str = Field(alias='university_name')
+    id: int = Field(alias='university_id')
+    parent_id: Optional[int] = Field(alias='university_parent_id')
+    short_name: str = Field(alias='university_short_name')
+    name_en: str = Field(alias='university_name_en')
+    is_from_crimea: str = Field(alias='is_from_crimea')
+    registration_year: Optional[int] = Field(alias='registration_year')
+    edrpou: int = Field(alias='university_edrpou')
+    type_name: str = Field(alias='university_type_name')
+    financing: str = Field(alias='university_financing_type_name')
+    governance: str = Field(alias='university_governance_type_name')
+    post_index: int = Field(alias='post_index')
+    koatuu_id: int = Field(alias='koatuu_id')
+    region_name: str
+    koatuu_name: str
+    address: str = Field(alias='university_address')
+    post_index_u: int = Field(alias='post_index_u')
+    koatuu_id_u: int = Field(alias='koatuu_id_u')
+    region_name_u: str = Field(alias='region_name_u')
+    koatuu_name_u: Optional[str] = Field(alias='koatuu_name_u')
+    address_u: str = Field(alias='university_address_u')
+    phone: str = Field(alias='university_phone')
+    email: str = Field(alias='university_email')
+    site: str = Field(alias='university_site')
+    director_post: str = Field(alias='university_director_post')
+    director_fio: str = Field(alias='university_director_fio')
+    close_date: Optional[str] = Field(alias='close_date')
+    primitki: str = Field(alias='primitki')
+
+
+def load_universities_from_every_region() -> None:
+    """
+    Runs through all the regions
+    Loads all the universities in each region
+    """
     region_json_names = os.listdir(UNIVERSITIES_PATH)
 
     for name in region_json_names:
@@ -64,7 +99,7 @@ def seed_db(data: List[dict], region: models.Regions) -> None:
         region.universities.add(un)
 
 
-# Run the `main` function from the Django shell (python3 manage.py shell)!!!
+# Call the `main` function from the Django shell (python3 manage.py shell)!!!
 def main() -> None:
     try:
         download_universities()
@@ -72,6 +107,4 @@ def main() -> None:
         print("Failed to download a certain university")
         exit(1)
 
-    process_regions()
-
-
+    load_universities_from_every_region()
